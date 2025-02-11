d50ed6864ef8034b9b2a2714b39ff7772e808d30
yvonne <yvonnewong1107@gmail.com>
2025-02-11 14:02:28 +0000

    scaling factor added for the gap between buttons

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ const noButton = document.getElementById("no-btn");
 const gif = document.getElementById("kuromi-gif");
 const yesButton = document.getElementById("yes-btn");
 const buttonContainer = document.querySelector(".btn-container");
+const scaleFactor = 230; 
 
 //array of gifs
 const gifs = ["./assets/images/kuromi-sad1.gif", "./assets/images/kuromi-sad2.gif", "./assets/images/kuromi-sad3.gif"];
 //array of messages
-const buttonMessages = ["Are you sure?", "Please?", "Please please please", "You can't do this to me!"];
-
+const buttonMessages = ["Are you sure?", "Really?", "Please?", "Please please please"];
 
 // when the no button is clicked
 noButton.addEventListener("click", () => {
@@ -24,7 +24,6 @@ noButton.addEventListener("click", () => {
 
     // changing no button text messages wrapping through from start to end of the array
     noButton.textContent = buttonMessages[clicks % maxButtonClicks];
-    
 
     // decrease the size of the no button
     if (noScale > smallestNoScale) {
@@ -32,12 +31,23 @@ noButton.addEventListener("click", () => {
         noButton.style.transform = `scale(${noScale})`;
     }
 
-    // decrease the size of the no button
+    // increase the size of the yes button
     if (yesScale < largestYesScale) {
         yesScale += 0.2;
         yesButton.style.transform = `scale(${yesScale})`;
-    }
 
+    // adjust gap of buttons dynamically
+    // access computed css gap property from button container
+    const computedStyle = getComputedStyle(buttonContainer);
+    const currentGap = parseFloat(computedStyle.gap);
+    
+    // Calculate new gap with square root scaling
+    let newGap = Math.sqrt(currentGap * scaleFactor);
+    
+    // update new gap to be current gap
+    buttonContainer.style.gap = `${newGap}px`;
+    }
+    
     // increment the number of clicks
     clicks++;
 });
